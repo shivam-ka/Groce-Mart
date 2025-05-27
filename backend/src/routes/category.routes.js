2ef8e9202e11dd9540d33a9b1702ba882d0c3247
Shivam_kashyap <shivamkabusiness.com>
2025-05-27 14:18:36 +0530

    change: name of upload category to add category

--- backend/src/routes/category.routes.js
+++ backend/src/routes/category.routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { uploadCategory } from "../controllers/category.controller.js";
+import { addCategory } from "../controllers/category.controller.js";
 import upload from "../middlewares/multer.middleware.js";
 import { authUser } from "../middlewares/auth.middleware.js";
 
 const categoryRouter = Router();
 
-categoryRouter.post('/add-category', authUser, upload.single('image'), uploadCategory)
+categoryRouter.post('/add-category', authUser, upload.single('image'), addCategory)
 
 export default categoryRouter
